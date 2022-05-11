68bc0c5575c779ae2390ea1a4baa079956bfe62a
RingLord54 <andrew.moneypenny@btinternet.com>
2022-05-11 18:42:53 +0100

    Update Utility.cs
    Comments Updated

--- Utility.cs
+++ Utility.cs
@@ -24,11 +24,18 @@ namespace TicTacToe
             return board;
         }
 
+        /// <summary>
+        /// This method is used to display the 3x3 Game Board to the User<br></br>
+        /// and updating it when spaces are filled in with either "X"s or "O"s
+        /// </summary>
+        /// <param name="board"> The Current Game Board </param>
         public static void display(string[][] board)
         {
+            // Numbers added to indicate numerical values associated with the columns
             Console.WriteLine("       0       1       2");
             for (int i = 0; i < board.Length; i++)
             {
+                // "i" is used on the 4th line to indicate the numerical values associated with the rows
                 Console.WriteLine("   +-------+-------+-------+");
                 Console.WriteLine("   |       |       |       |");
                 Console.WriteLine("   |       |       |       |");
@@ -39,22 +46,26 @@ namespace TicTacToe
             }
         }
 
+
+
         /// <summary>
         /// This method is used to ensure that if the 3x3 Board is filled up<br></br>
         /// then the game doesn't keep going and instead is ended and is marked<br></br>
         /// as a draw at that point.
         /// </summary>
-        /// <param name="board"> The Game board </param>
+        /// <param name="board"> The Current Game board </param>
         /// <returns>
         /// If the Board is full - return true<br></br>
         /// If the Board idn't full - return false
         /// </returns>
         public static Boolean boardFull(string[][] board)
         {
+            // Loops through all of the nine board spaces
             for (int i = 0; i < board.Length; i++)
             {
                 for (int j = 0; j < board[i].Length; j++)
                 {
+                    // If one of the spaces is empty (No X or O) then it's not full
                     if (board[i][j] != "X" || board[i][j] != "O") {
                         return false;
                     }
@@ -63,11 +74,13 @@ namespace TicTacToe
             return true;
         }
 
+
+
         /// <summary>
         /// This method is used in order to check to see if either the Player<br></br>
         /// or the Opponent has won the game after a move is made. 
         /// </summary>
-        /// <param name="board"> The Game Board </param>
+        /// <param name="board"> The Current Game Board </param>
         /// <param name="value"> Either "X" or "O", and used to check who's won</param>
         /// <returns>
         /// If the Player / Opponent has won - return true<br></br>
@@ -75,6 +88,11 @@ namespace TicTacToe
         /// </returns>
         public static Boolean gameWon(string[][] board, string value)
         {
+            /* Checks all of the possible winning combinations against either the "X" character or the "O" character
+             * depending on the value of the "value" parameter, and if a winning combination is found then it returns
+             * true, meaning either the Player or Opponent has won the game
+             */
+
             if (board[0][0].Equals(value) && board[0][1].Equals(value) && board[0][2].Equals(value)) { return true; }
             else if (board[1][0].Equals(value) && board[1][1].Equals(value) && board[1][2].Equals(value)) { return true; }
             else if (board[2][0].Equals(value) && board[2][1].Equals(value) && board[2][2].Equals(value)) { return true; }
